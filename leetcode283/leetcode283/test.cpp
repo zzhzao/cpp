c93885c1214cae4a03ae65942c8e9c9b279a71f0
zzhZAO <3516422381@qq.com>
2025-06-17 10:40:58 +0800

    移动零进阶写法，排序算法思想

--- leetcode283/leetcode283/test.cpp
+++ leetcode283/leetcode283/test.cpp
@@ -2,16 +2,22 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int dest = -1;
-        int cur = 0;
-        while (cur < nums.size())
+        //int dest = -1;
+        //int cur = 0;
+        //while (cur < nums.size())
+        //{
+        //    if (nums[cur] != 0)
+        //    {
+        //        dest++;
+        //        swap(nums[dest], nums[cur]);
+        //    }
+        //    cur++;
+        for(int dest = -1,int cur = 0;cur<nums.size();cur++)
         {
             if (nums[cur] != 0)
             {
-                dest++;
-                swap(nums[dest], nums[cur]);
+                swap(nums[++dest], nums[cur]);
             }
-            cur++;
         }
     }
 };
