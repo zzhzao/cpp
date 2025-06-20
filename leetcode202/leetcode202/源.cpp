03b3c7e0de6d27b4fe9446f5f9606f7cd67a8dea
zzhZAO <3516422381@qq.com>
2025-06-20 16:49:29 +0800

    leetcode202快乐数

---
+++ leetcode202/leetcode202/源.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+class Solution {
+public:
+    int bitSquareSum(int n) {
+        int sum = 0;
+        while (n > 0)
+        {
+            int bit = n % 10;
+            sum += bit * bit;
+            n = n / 10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        int slow = n, fast = n;
+        do {
+            slow = bitSquareSum(slow);
+            fast = bitSquareSum(fast);
+            fast = bitSquareSum(fast);
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+};
+
