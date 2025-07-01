9eb2740ff91806c56278afdd2012f5c336100750
zzhZAO <3516422381@qq.com>
2025-07-01 08:06:39 +0800

    异常

---
+++ 异常/异常/源.cpp
@@ -0,0 +1,164 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>
+#include<thread>
+using namespace std;
+class Exception
+
+{
+
+public:
+	Exception(const string& errmsg, int id)
+		:_errmsg(errmsg)
+		, _id(id)
+	{
+	}
+	virtual string what() const
+
+	{
+		return _errmsg;
+	}
+	int getid() const
+
+	{
+		return _id;
+	}
+
+protected:
+	string _errmsg;
+	int _id;
+};
+
+class SqlException : public Exception
+{
+
+public:
+	SqlException(const string& errmsg, int id, const string& sql)
+		:Exception(errmsg, id)
+		, _sql(sql)
+	{
+	}
+	virtual string what() const
+
+	{
+		string str = "SqlException:";
+		str += _errmsg;
+		str += "->";
+		str += _sql;
+		return str;
+	}
+
+private:
+	const string _sql;
+};
+
+class CacheException : public Exception
+{
+
+public:
+	CacheException(const string& errmsg, int id)
+		:Exception(errmsg, id)
+	{
+	}
+	virtual string what() const
+
+	{
+		string str = "CacheException:";
+		str += _errmsg;
+		return str;
+	}
+};
+
+class HttpException : public Exception
+{
+
+public:
+	HttpException(const string& errmsg, int id, const string& type)
+		:Exception(errmsg, id)
+		, _type(type)
+	{
+	}
+	virtual string what() const
+
+	{
+		string str = "HttpException:";
+		str += _type;
+		str += ":";
+		str += _errmsg;
+		return str;
+	}
+
+private:
+	const string _type;
+};
+
+void SQLMgr()
+{
+	if (rand() % 7 == 0)
+	{
+		throw SqlException("权限不⾜", 100, "select * from name = '张三'");
+	}
+	else
+
+	{
+		cout << "SQLMgr 调用成功" << endl;
+	}
+}
+
+void CacheMgr()
+{
+	if (rand() % 5 == 0)
+	{
+		throw CacheException("权限不足", 100);
+	}
+	else if (rand() % 6 == 0)
+	{
+		throw CacheException("数据不存在", 101);
+	}
+	else
+
+	{
+		cout << "CacheMgr 调用成功" << endl;
+	}
+	SQLMgr();
+}
+
+void HttpServer()
+{
+	if (rand() % 3 == 0)
+	{
+		throw HttpException("请求资源不存在", 100, "get");
+	}
+	else if (rand() % 4 == 0)
+	{
+		throw HttpException("权限不足", 101, "post");
+	}
+	else
+
+	{
+		cout << "HttpServer调用成功" << endl;
+	}
+	CacheMgr();
+}
+
+int main()
+{
+	srand(time(0));
+	while (1)
+	{
+		this_thread::sleep_for(chrono::seconds(1));
+		try
+		{
+			HttpServer();
+		}
+		catch (const Exception& e) // 这⾥捕获基类，基类对象和派⽣类对象都可以被捕获
+		{
+		cout << e.what() << endl;
+		}
+			catch (...)
+		{
+			cout << "Unkown Exception" << endl;
+		}
+	}
+	return 0;
+}
