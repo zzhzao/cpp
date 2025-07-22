225278f6a5e5d625ab8cf65430c3185b1115cab5
zzhZAO <3516422381@qq.com>
2025-07-22 17:31:21 +0800

    leetcode438寻找字符串中所有字母的异位词

--- leetcode438/leetcode438/源.cpp
+++ leetcode438/leetcode438/源.cpp
@@ -1,35 +1,23 @@
-#define _CRT_SECURE_NO_WARNINGS 1
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        int sLen = s.size(), pLen = p.size();
+        vector<int> ret;
+        int hash1[26] = { 0 };
+        for (auto ch : p) hash1[ch - 'a']++;
 
-        if (sLen < pLen) {
-            return vector<int>();
-        }
-
-        vector<int> ans;
-        vector<int> sCount(26);
-        vector<int> pCount(26);
-        for (int i = 0; i < pLen; ++i) {
-            ++sCount[s[i] - 'a'];
-            ++pCount[p[i] - 'a'];
-        }
-
-        if (sCount == pCount) {
-            ans.emplace_back(0);
-        }
-
-        for (int i = 0; i < sLen - pLen; ++i) {
-            --sCount[s[i] - 'a'];
-            ++sCount[s[i + pLen] - 'a'];
-
-            if (sCount == pCount) {
-                ans.emplace_back(i + 1);
+        int hash2[26] = { 0 };
+        int m = p.size();
+        for (int left = 0, right = 0, count = 0;right < s.size();right++)
+        {
+            char in = s[right];
+            if (++hash2[in - 'a'] <= hash1[in - 'a'])  count++;
+            if (right - left + 1 > m)
+            {
+                char out = s[left++];
+                if (hash2[out - 'a']-- <= hash1[out - 'a'])  count--;
             }
+            if (count == m) ret.push_back(left);
         }
-
-        return ans;
+        return ret;
     }
 };
-
